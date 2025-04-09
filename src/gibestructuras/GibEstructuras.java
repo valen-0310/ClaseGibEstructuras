ab8fa03daf252d79bed01671be341c6ca9774288
miUsu <josekber1@gmail.com>
2025-04-09 18:57:29 -0500

    este es para probar y es comentario

---
+++ src/gibestructuras/GibEstructuras.java
@@ -0,0 +1,15 @@
+
+package gibestructuras;
+
+
+public class GibEstructuras {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hice un cambio es este");
+    }
+    
+}
